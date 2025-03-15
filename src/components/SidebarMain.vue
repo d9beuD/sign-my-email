b97394acd347f1e32c0cf07980645657212f7169
d9beuD <d9beuD@users.noreply.github.com>
2025-03-15 23:30:41 +0100

    Add form

--- src/components/SidebarMain.vue
+++ src/components/SidebarMain.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-import { faSignature } from '@fortawesome/pro-regular-svg-icons'
+import { useSignatureStore } from '@/stores/signature'
+import { faSignature, faSuitcase, faUser } from '@fortawesome/pro-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
+
+const signatureStore = useSignatureStore()
 </script>
 
 <template>
@@ -9,8 +15,80 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
       <FontAwesomeIcon :icon="faSignature" />
       <span>Sign My Email</span>
     </h1>
-    <div class="">
-      <h2 class="border-b">Personal Info</h2>
-    </div>
+
+    <section>
+      <h2 class="mb-2 border-b pb-2">
+        <FontAwesomeIcon :icon="faUser" />
+        Personal Info
+      </h2>
+
+      <div class="mb-4">
+        <Label for="personalName">Name</Label>
+        <Input v-model="signatureStore.personalInfo.name" placeholder="Name" id="personalName" />
+      </div>
+
+      <div class="mb-4">
+        <Label for="personalJobTitle">Job Title</Label>
+        <Input
+          v-model="signatureStore.personalInfo.jobTitle"
+          placeholder="Job Title"
+          id="personalJobTitle"
+        />
+      </div>
+
+      <div class="mb-4">
+        <Label for="personalDepartment">Department</Label>
+        <Input
+          v-model="signatureStore.personalInfo.department"
+          placeholder="Department"
+          id="personalDepartment"
+        />
+      </div>
+
+      <div class="mb-4">
+        <Label for="personalEmailAddress">Email Address</Label>
+        <Input
+          v-model="signatureStore.personalInfo.email"
+          placeholder="Email Address"
+          id="personalEmailAddress"
+          type="email"
+        />
+      </div>
+    </section>
+
+    <section>
+      <h2 class="mb-2 border-b pb-2">
+        <FontAwesomeIcon :icon="faSuitcase" />
+        Business Info
+      </h2>
+
+      <div class="mb-4">
+        <Label for="businessName">Company Name</Label>
+        <Input
+          v-model="signatureStore.businessInfo.companyName"
+          placeholder="Company Name"
+          id="businessName"
+        />
+      </div>
+
+      <div class="mb-4">
+        <Label for="businessAddress">Address</Label>
+        <Textarea
+          v-model="signatureStore.businessInfo.address"
+          placeholder="Address"
+          id="businessAddress"
+          rows="3"
+        />
+      </div>
+
+      <div class="mb-4">
+        <Label for="businessWebsite">Website</Label>
+        <Input
+          v-model="signatureStore.businessInfo.website"
+          placeholder="Website"
+          id="businessWebsite"
+        />
+      </div>
+    </section>
   </div>
 </template>
