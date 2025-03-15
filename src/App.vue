a526c813c514c3e96d980e597c47da7c1f776f67
d9beuD <d9beuD@users.noreply.github.com>
2025-03-15 20:19:29 +0100

    Add Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import SidebarMain from './components/SidebarMain.vue'
 </script>
 
 <template>
+  <div id="app-container" class="flex h-dvh w-dvw bg-sky-100">
+    <div class="h-full w-96 border-e bg-background">
+      <div class="p-4">
+        <SidebarMain />
+      </div>
     </div>
 
 </template>
---
+++ src/components/SidebarMain.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col">
+    <h1 class="mb-4 text-lg">Sign My Email</h1>
+    <div class="">
+      <h2 class="border-b">Personal Info</h2>
+    </div>
+  </div>
+</template>
