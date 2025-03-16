9d2e274859ff1aee9c2b25aa25d181e2e73f48ea
d9beuD <d9beuD@users.noreply.github.com>
2025-03-16 12:22:57 +0100

    Add ability to choose a template from selection

--- src/components/SidebarMain.vue
+++ src/components/SidebarMain.vue
@@ -1,21 +1,51 @@
 <script setup lang="ts">
 import { useSignatureStore } from '@/stores/signature'
-import { faSignature, faSuitcase, faUser } from '@fortawesome/pro-regular-svg-icons'
+import { faShapes, faSignature, faSuitcase, faUser } from '@fortawesome/pro-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import FormGroup from './FormGroup.vue'
 import SidebarSection from './SidebarSection.vue'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select'
+import { templates } from './template'
 
 const signatureStore = useSignatureStore()
+const availableTemplates = templates
 </script>
 
 <template>
   <div class="flex flex-col space-y-4">
     <h1 class="flex items-center gap-x-2 text-lg font-bold text-primary">
-      <FontAwesomeIcon :icon="faSignature" />
+      <FontAwesomeIcon :icon="faSignature" size="lg" />
       <span>Sign My Email</span>
     </h1>
+
+    <SidebarSection :icon="faShapes" title="Template">
+      <FormGroup for="templateSelector" label="Template">
+        <Select v-model="signatureStore.template">
+          <SelectTrigger>
+            <SelectValue placeholder="Select a template" />
+          </SelectTrigger>
+
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Templates</SelectLabel>
+              <template v-for="template in availableTemplates" :key="template.name">
+                <SelectItem :value="template">{{ template.name }}</SelectItem>
+              </template>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </FormGroup>
+    </SidebarSection>
 
     <SidebarSection :icon="faUser" title="Personal Info">
       <FormGroup for="personalName" label="Name">
