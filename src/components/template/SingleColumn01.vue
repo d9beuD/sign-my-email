9ba4d5db615ee9c41240be1a491791acec2b8e4f
d9beuD <d9beuD@users.noreply.github.com>
2025-03-17 11:59:30 +0100

    Add SingleColumn01 template

--- src/components/template/SingleColumn01.vue
+++ src/components/template/SingleColumn01.vue
@@ -1,8 +1,114 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import { useSignatureStore } from '@/stores/signature'
+import { faEnvelope, faLink, faLocationDot } from '@fortawesome/pro-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { computed } from 'vue'
+import TemplateBase from './TemplateBase.vue'
+
+const signatureStore = useSignatureStore()
+const primaryColor = '#3b82f6'
+const iconCellStyle = 'padding-right: 8px; margin-right: 1px; vertical-align: top;'
+
+const showFirstSection = computed(
+  () =>
+    signatureStore.personalInfo.name ||
+    signatureStore.personalInfo.jobTitle ||
+    signatureStore.personalInfo.department ||
+    signatureStore.businessInfo.companyName,
+)
+const showSecondSection = computed(
+  () =>
+    signatureStore.personalInfo.email ||
+    signatureStore.personalInfo.phoneNumbers.length ||
+    signatureStore.businessInfo.website ||
+    signatureStore.businessInfo.address,
+)
+
+const businessAddress = computed(() =>
+  signatureStore.businessInfo.address?.replace(/(?:\r\n|\r|\n)/g, '<br>'),
+)
 </script>
 
 <template>
-  <div>
-  </div>
+  <TemplateBase>
+    <tr v-if="showFirstSection">
+      <td>
+        <div v-if="signatureStore.personalInfo.pictureUrl" style="padding-bottom: 8px">
+          <img
+            :src="signatureStore.personalInfo.pictureUrl"
+            alt=""
+            :style="`width: ${signatureStore.personalInfo.pictureWidth}px`"
+          />
+        </div>
+        <div v-if="signatureStore.personalInfo.name" style="font-weight: bold; margin-bottom: 8px">
+          {{ signatureStore.personalInfo.name }}
+        </div>
+        <div v-if="signatureStore.personalInfo.jobTitle">
+          {{ signatureStore.personalInfo.jobTitle }}
+        </div>
+        <div v-if="signatureStore.personalInfo.department">
+          {{ signatureStore.personalInfo.department }}
+        </div>
+        <div v-if="signatureStore.businessInfo.companyName">
+          {{ signatureStore.businessInfo.companyName }}
+        </div>
+      </td>
+    </tr>
+
+    <tr v-if="showFirstSection && showSecondSection">
+      <td style="padding: 16px 0">
+        <div :style="`border-top: 2px solid ${primaryColor};`"></div>
+      </td>
+    </tr>
+
+    <tr v-if="showSecondSection">
+      <td>
+        <div v-if="signatureStore.personalInfo.email" style="margin-bottom: 8px">
+          <table>
+            <tbody>
+              <tr>
+                <td :style="iconCellStyle">
+                  <FontAwesomeIcon :icon="faEnvelope" fixed-width />
+                </td>
+                <td>
+                  {{ signatureStore.personalInfo.email }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div v-if="signatureStore.businessInfo.address" style="margin-bottom: 8px">
+          <table>
+            <tbody>
+              <tr>
+                <td :style="iconCellStyle">
+                  <FontAwesomeIcon :icon="faLocationDot" fixed-width />
+                </td>
+                <td>
+                  <div v-html="businessAddress"></div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div v-if="signatureStore.businessInfo.website" style="margin-bottom: 8px">
+          <table>
+            <tbody>
+              <tr>
+                <td :style="iconCellStyle">
+                  <FontAwesomeIcon :icon="faLink" fixed-width />
+                </td>
+                <td>
+                  <a :href="signatureStore.businessInfo.website">{{
+                    signatureStore.businessInfo.website
+                  }}</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </td>
+    </tr>
+  </TemplateBase>
 </template>
---
+++ src/components/template/TemplateBase.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const resetLinks = '<style>.reset-links a { text-decoration: none !important; }</style>'
+</script>
+
+<template>
+  <div v-html="resetLinks"></div>
+  <table
+    cellpadding="0"
+    cellspacing="0"
+    border="0"
+    class="reset-links"
+    style="margin: 0px; border-collapse: collapse; font-size: 16px; line-height: 16px"
+  >
+    <tbody>
+      <slot></slot>
+    </tbody>
+  </table>
+</template>
