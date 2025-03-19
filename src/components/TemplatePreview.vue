ddfba13ecbdc41ca04e9927886c4f47b7e0e4533
d9beuD <d9beuD@users.noreply.github.com>
2025-03-19 23:37:23 +0100

    Make signature div queryable

--- src/components/TemplatePreview.vue
+++ src/components/TemplatePreview.vue
@@ -5,7 +5,7 @@ const signatureStore = useSignatureStore()
 </script>
 
 <template>
-  <div class="pt-4 text-foreground">
+  <div class="pt-4 text-foreground" id="rendered-signature">
     <component v-if="signatureStore.template" :is="signatureStore.template.template" />
   </div>
 </template>
