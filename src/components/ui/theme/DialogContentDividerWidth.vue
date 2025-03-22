2966cae13ef437e16d86df772477007bc214e4ea
d9beuD <d9beuD@users.noreply.github.com>
2025-03-22 16:26:37 +0100

    Show divider width preview when editing

--- src/components/ui/theme/DialogContentDividerWidth.vue
+++ src/components/ui/theme/DialogContentDividerWidth.vue
@@ -8,6 +8,8 @@ import {
   NumberFieldIncrement,
   NumberFieldInput,
 } from '../number-field'
+import { Skeleton } from '../skeleton'
+import TemplateDivider from '@/components/template/TemplateDivider.vue'
 
 const signatureStore = useSignatureStore()
 </script>
@@ -17,6 +19,17 @@ const signatureStore = useSignatureStore()
     <DialogHeader>
       <DialogTitle>Edit Dividers width</DialogTitle>
       <DialogDescription> This is the divider width in <code>px</code> unit. </DialogDescription>
+
+      <div class="flex items-center gap-2">
+        <Skeleton class="size-12 rounded-full" />
+
+        <TemplateDivider orientation="vertical" />
+
+        <div class="flex grow flex-col gap-2">
+          <Skeleton class="h-4 w-8/12" />
+          <Skeleton class="h-4 w-6/12" />
+        </div>
+      </div>
     </DialogHeader>
 
     <NumberField v-model="signatureStore.themeOptions.divider.width" :min="1">
